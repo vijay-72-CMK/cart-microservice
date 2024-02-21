5399dbddaf0ad1f855e16eb573951f22db5778a9
Raswanth <rv@fastenal.com>
2024-02-21 10:56:38 +0530

    Initial project structure and files

---
+++ src/main/java/com/raswanth/cartmicroservice/CartMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.raswanth.cartmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/raswanth/cartmicroservice/exception/GeneralInternalException.java
@@ -0,0 +1,21 @@
+package com.raswanth.cartmicroservice.exception;
+
+
+import org.springframework.http.HttpStatus;
+
+public class GeneralInternalException extends RuntimeException {
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    private HttpStatus httpStatus;
+    public GeneralInternalException(String message, HttpStatus status) {
+        super(message);
+        httpStatus = status;
+    }
+
+    public GeneralInternalException(String message) {
+        super(message);
+        httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+}
